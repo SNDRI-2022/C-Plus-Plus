980f71da7b23833d5ecc4e62d574107febea39ca
liu2830 <2305195328@qq.com>
2022-11-03 11:35:39 +0800

    modified:   output/main.exe 	new file:   test/test.cpp 	new file:   test/test.exe 	new file:   test/test.hpp

---
+++ test/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <test.hpp>
+using namespace std;
+
+int main()
+{
+    std::cout << "maintest" << std::endl;
+    return 0;
+}
---
+++ test/test.hpp
@@ -0,0 +1,5 @@
+#include <iostream>
+void testfunc()
+{
+    std::cout << "test" << std::endl;
+}
