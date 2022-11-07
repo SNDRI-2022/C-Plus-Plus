f36f1799a52cf95e06714f2352924e20803b2ce2
liu2830 <2305195328@qq.com>
2022-11-07 15:16:45 +0800

    modified:   include/sort-InsertionSort.hpp

--- include/sort-InsertionSort.hpp
+++ include/sort-InsertionSort.hpp
@@ -12,3 +12,17 @@
 void MyInsertionSort(MyArray &_array)
 {
 }
+
+/********************************************************************************
+ * @brief
+ * ! void insertion_sort(int arr[], int len)
+ * ! {
+ * ! int i,j,temp;
+ * ! for (i=1;i<len;i++){
+ * !          temp = arr[i];
+ * !           for (j=i;j>0 && arr[j-1]>temp;j--)
+ * !                   arr[j] = arr[j-1];
+ * !           arr[j] = temp;
+ * ! }
+ * !}
+ ********************************************************************************/
